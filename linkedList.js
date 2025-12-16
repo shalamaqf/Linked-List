41149001b38a6c0a80edd5062b4564be787b76d9
Shalama <shalamafadillaa@gmail.com>
2025-12-16 13:22:45 +0700

    Move the node class inside linkedList.js

--- linkedList.js
+++ linkedList.js
@@ -1,5 +1,14 @@
+// Create a class for a list
 export class LinkedList {
     constructor() {
         this.head = null;
+    }
+}
+
+// Creat a class for a node
+export class Node {
+    constructor() {
+        this.value = null;
+        this.next = null;
     }
 }

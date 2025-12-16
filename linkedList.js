19b80ac61798a1ed54b6ef2dd05814fa7808ab5e
Shalama <shalamafadillaa@gmail.com>
2025-12-16 13:52:27 +0700

    Create a method to add new node to the end of list

--- linkedList.js
+++ linkedList.js
@@ -4,6 +4,24 @@ export class LinkedList {
         this.head = null;
         this.tail = null;
     }
+
+    // Create a method to append a node to the end of the list
+    append(value) {
+        // Create the new node and set its properties
+        const newNode = new Node();
+        newNode.value = value;
+        newNode.next = null;
+
+        // Set the node as head and tail if the list is empty
+        if (list.head === null && list.tail === null) {
+            list.head = newNode;
+            list.tail = newNode;
+        } else {
+            // Else, set the tail to the new node
+            list.tail.next = newNode;
+            list.tail = newNode;
+        }
+    }
 }
 
 // Creat a class for a node
